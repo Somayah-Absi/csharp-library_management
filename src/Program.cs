91c0d49b265ba7ff621bf1eba9923ddb0d893217
Somayah-Absi <somayahahmedabsi@gmail.com>
2024-04-15 12:34:08 +0300

    starting

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,44 @@
-﻿internal class Program
+﻿using System;
+internal class Program
 {
-    private static void Main()
+
+
+    public abstract class Information
+{
+        public Guid Id;
+        public DateTime CreatedDate;
+
+    public Information()
+    {
+        Id = Guid.NewGuid();
+        CreatedDate = DateTime.Now;
+    }
+}
+
+// Book class
+public class Book : Information
+{
+    public string Title { get; }
+
+    public Book(string title) : base()
+    {
+        Title = title;
+    }
+}
+
+// User class
+public class User : Information
+{
+    public string Name { get; }
+
+    public User(string name) : base()
+    {
+        Name = name;
+    }
+}
+
+
+    public static void Main()
     {
     }
 }
